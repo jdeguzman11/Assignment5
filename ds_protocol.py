b9a9a0e2a9daed17e3db90d6ddcfe98352326431
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-03-13 22:25:47 -0700

    Implemented working ds_protocol from Assignment4.

--- ds_protocol.py
+++ ds_protocol.py
@@ -3,3 +3,27 @@
 # 72329664
 
 # ds_protocol.py
+
+""""""
+
+import json
+from collections import namedtuple
+
+DataTuple = namedtuple("DataTuple", ["type", "token"])
+
+
+def extract_json(json_msg: str) -> DataTuple:
+    """Utility functions for handling DS server JSON responses."""
+    try:
+        json_obj = json.loads(json_msg)
+        response_type = json_obj["response"]["type"]
+        token = json_obj["response"].get("token", "")
+
+    except json.JSONDecodeError:
+        print("Json cannot be decoded.")
+        return DataTuple(None, None)
+
+    except (KeyError, TypeError):
+        return DataTuple(None, None)
+
+    return DataTuple(response_type, token)
