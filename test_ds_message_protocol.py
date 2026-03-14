567d48c49c77f6f2354135bf3eb1b6a9c09c9e2d
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-03-14 14:48:44 -0700

    Created unit test for extract_json.

--- test_ds_message_protocol.py
+++ test_ds_message_protocol.py
@@ -3,3 +3,19 @@
 # 72329664
 
 # test_ds_message_protocol.py
+
+"""Unit tests for ds_protocol file."""
+
+import ds_protocol
+
+
+def test_extract_json():
+    """Unit test for handling valid JSON responses from DS server."""
+    json_msg = '''
+    {"response": {"type": "ok", "token": "example_token"}}
+    '''
+
+    result = ds_protocol.extract_json(json_msg)
+
+    assert result.type == "ok"
+    assert result.token == "example_token"
