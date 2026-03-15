500365442212bcca3347f24868d326742caf46dd
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-03-14 22:42:49 -0700

    Created unit test for DirectMessage initialization.

--- test_ds_messenger.py
+++ test_ds_messenger.py
@@ -3,3 +3,16 @@
 # 72329664
 
 # test_ds_messenger.py
+
+"""Unit tests for ds_messenger file."""
+
+from ds_messenger import DirectMessage, DirectMessenger
+
+
+def test_direct_message_init():
+    """Testing DirectMessage initialization."""
+    msg = DirectMessage("jdawg", "SWE", "711")
+
+    assert msg.recipient == "jdawg"
+    assert msg.message == "SWE"
+    assert msg.timestamp == "711"
