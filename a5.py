4b37694bb6248f10822a45203e74c97bb7d47b4c
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-03-18 12:36:47 -0700

    Created basic Tkinter window.

--- a5.py
+++ a5.py
@@ -3,3 +3,16 @@
 # 72329664
 
 # a5.py
+
+import tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    root.title("DS Messenger")
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
